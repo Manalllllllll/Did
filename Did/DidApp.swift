a878c95f047754e32803d61d810e84f6274c555c
Manalllllllll <manalebraheem072@gmail.com>
2023-10-09 16:39:47 +0300

    Initial Commit

---
+++ Did/DidApp.swift
@@ -0,0 +1,32 @@
+//
+//  DidApp.swift
+//  Did
+//
+//  Created by silva on 24/03/1445 AH.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DidApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
