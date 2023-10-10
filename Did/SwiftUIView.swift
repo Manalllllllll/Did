eba8d50f03b6a1e080a93e833365cb4a1b5acc78
aasalhamied@gmail.com <aasalhamied@gmail.com>
2023-10-10 14:12:06 +0300

    update

---
+++ Did/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Did
+//
+//  Created by alhanouf alhamied on 24/03/1445 AH.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
