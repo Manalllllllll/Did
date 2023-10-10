ef2dfb5dea2d1aa6af5cda9ba3ecb32574fdd6e3
Taif0bader <taifbaderalharbi@gmail.com>
2023-10-10 14:11:50 +0300

    sheet changes

---
+++ Did/sheets.swift
@@ -0,0 +1,18 @@
+//
+//  sheets.swift
+//  Did
+//
+//  Created by Taif Alharbi on 25/03/1445 AH.
+//
+
+import SwiftUI
+
+struct sheets: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sheets()
+}
