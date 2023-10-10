d39a45f150d970020e5a121f75bc1b3d020d0582
aasalhamied@gmail.com <aasalhamied@gmail.com>
2023-10-10 15:01:03 +0300

    page

---
+++ Did/face.swift
@@ -0,0 +1,18 @@
+//
+//  face.swift
+//  Did
+//
+//  Created by alhanouf alhamied on 25/03/1445 AH.
+//
+
+import SwiftUI
+
+struct face: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    face()
+}
